4ca685ee48cb6745d7bc5dad2c147ca7805fbc52
TanguyChiffoleau <55456592+TanguyChiffoleau@users.noreply.github.com>
2020-07-15 22:52:12 +0200

    Ajout dépendance dotenv + adaptation app en fonction de l'environnement (production/dev)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const Discord = require('discord.js')
 const { events: eventsLoader, commands: commandsLoader } = require('./util/loaders')
 
+if (process.env.NODE_ENV !== 'production') require('dotenv').config({ path: './config/bot.env' })
+
 const client = new Discord.Client()
 client.commands = new Discord.Collection()
-
 client.prefix = '!'
 
-client.login(process.env.TOKEN)
+client.login(process.env.DISCORD_TOKEN)
 
 eventsLoader(client)
 
