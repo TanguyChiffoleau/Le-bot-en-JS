743f82963571523ada027fc1c8282076f64faa8b
TanguyChiffoleau <55456592+TanguyChiffoleau@users.noreply.github.com>
2020-07-20 17:25:38 +0200

    Ajout fichier util avec la fonction convertDate

---
+++ src/util/util.js
@@ -0,0 +1,15 @@
+module.exports = {
+	convertDate: date =>
+		`${date.getDate().toString().padStart(2, '0')}/${(date.getMonth() + 1)
+			.toString()
+			.padStart(2, '0')}/${date
+			.getFullYear()
+			.toString()
+			.padStart(4, '0')} ${date
+			.getHours()
+			.toString()
+			.padStart(2, '0')}:${date
+			.getMinutes()
+			.toString()
+			.padStart(2, '0')}:${date.getSeconds().toString().padStart(2, '0')}`,
+}
