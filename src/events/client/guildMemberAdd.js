25aa5838ff8ace57e58a908befde46b28de678ea
TanguyChiffoleau <55456592+TanguyChiffoleau@users.noreply.github.com>
2020-07-20 18:51:44 +0200

    Fix de l'oubli de restreindre le bot à une guild

--- src/events/client/guildMemberAdd.js
+++ src/events/client/guildMemberAdd.js
@@ -1,6 +1,8 @@
 const { convertDate } = require('../../util/util')
 
 module.exports = (cient, guildMember) => {
+	if (guildMember.guild.id !== process.env.GUILD_ID) return
+
 	const leaveJoinChannelId = process.env.LEAVE_JOIN_CHANNEL_ID
 	const leaveJoinChannel = guildMember.guild.channels.cache.find(
 		channel => channel.id === leaveJoinChannelId,
