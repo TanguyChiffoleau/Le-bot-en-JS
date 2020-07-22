a6898c3a9d54b0c678fd95f97f62becb9d725760
TanguyChiffoleau <55456592+TanguyChiffoleau@users.noreply.github.com>
2020-07-22 21:32:02 +0200

    Réduction longueur du code et simplification pour le report

--- src/events/client/messageReactionAdd.js
+++ src/events/client/messageReactionAdd.js
@@ -1,3 +1,4 @@
+/* eslint-disable consistent-return */
 // eslint-disable-next-line no-unused-vars
 const Discord = require('discord.js')
 const { convertDate } = require('../../util/util')
@@ -12,13 +13,9 @@ module.exports = async (client, messageReaction, user) => {
 	if (messageReaction.partial) await messageReaction.fetch().catch()
 	const { message, emoji } = messageReaction
 
-	if (
-		user.bot ||
-		emoji.name !== '🚨' ||
-		message.channel.type !== 'text' ||
-		message.author === user
-	)
-		return
+	if (user.bot || emoji.name !== '🚨' || message.channel.type !== 'text') return
+
+	if (message.author === user) return messageReaction.remove()
 
 	const reportChannel = message.guild.channels.cache.find(
 		channel => channel.id === process.env.REPORT_CHANNEL,
@@ -30,62 +27,39 @@ module.exports = async (client, messageReaction, user) => {
 		msg.embeds[0].fields.find(field => field.value.includes(message.id)),
 	)
 
-	if (logReport && logReport.embeds[0].fields.some(field => field.value.includes(user.id))) return
-
-	if (logReport)
+	if (logReport) {
+		if (logReport.embeds[0].fields.some(field => field.value.includes(user.id))) return
+		const editLogReport = {
+			embed: {
+				author: logReport.embeds[0].author,
+				description: logReport.embeds[0].description,
+				fields: [logReport.embeds[0].fields],
+				footer: logReport.embeds[0].footer,
+			},
+		}
 		switch (logReport.embeds[0].fields.length - 2) {
 			case 1:
-				logReport.edit({
-					embed: {
-						color: 'ff8200',
-						author: logReport.embeds[0].author,
-						description: logReport.embeds[0].description,
-						fields: [
-							logReport.embeds[0].fields,
-							{
-								name: '2nd signalement',
-								value: `Signalement de <@${user.id}> le ${convertDate(new Date())}`,
-								inline: false,
-							},
-						],
-						footer: logReport.embeds[0].footer,
-					},
+				editLogReport.embed.color = 'ff8200'
+				editLogReport.embed.fields.push({
+					name: '2nd signalement',
+					value: `Signalement de <@${user.id}> le ${convertDate(new Date())}`,
+					inline: false,
 				})
 				break
 			case 2:
-				logReport.edit({
-					embed: {
-						color: 'ff6600',
-						author: logReport.embeds[0].author,
-						description: logReport.embeds[0].description,
-						fields: [
-							logReport.embeds[0].fields,
-							{
-								name: '3ème signalement',
-								value: `Signalement de <@${user.id}> le ${convertDate(new Date())}`,
-								inline: false,
-							},
-						],
-						footer: logReport.embeds[0].footer,
-					},
+				editLogReport.embed.color = 'ff6600'
+				editLogReport.embed.fields.push({
+					name: '3ème signalement',
+					value: `Signalement de <@${user.id}> le ${convertDate(new Date())}`,
+					inline: false,
 				})
 				break
 			case 3:
-				logReport.edit({
-					embed: {
-						color: 'ff3200',
-						author: logReport.embeds[0].author,
-						description: logReport.embeds[0].description,
-						fields: [
-							logReport.embeds[0].fields,
-							{
-								name: '4ème signalement',
-								value: `Signalement de <@${user.id}> le ${convertDate(new Date())}`,
-								inline: false,
-							},
-						],
-						footer: logReport.embeds[0].footer,
-					},
+				editLogReport.embed.color = 'ff3200'
+				editLogReport.embed.fields.push({
+					name: '4ème signalement',
+					value: `Signalement de <@${user.id}> le ${convertDate(new Date())}`,
+					inline: false,
 				})
 				messageReaction.message.delete()
 				break
@@ -93,169 +67,106 @@ module.exports = async (client, messageReaction, user) => {
 				messageReaction.message.delete()
 				break
 		}
-	else
+		logReport.edit(editLogReport)
+	} else {
+		const sendLogReport = {
+			embed: {
+				author: {
+					name: `${message.author.tag} (ID ${message.author.id})`,
+					icon_url: message.author.displayAvatarURL({ dynamic: true }),
+				},
+				description: `**Contenu du message**\n${message.cleanContent}`,
+				fields: [
+					{
+						name: 'Channel',
+						value: message.channel,
+						inline: true,
+					},
+					{
+						name: 'Message',
+						value: `[${message.id}](${message.url})`,
+						inline: true,
+					},
+				],
+				footer: {
+					text: `Posté le ${convertDate(message.createdAt)}`,
+				},
+			},
+		}
 		switch (messageReaction.count) {
 			case 1:
-				reportChannel.send({
-					embed: {
-						color: 'ffae00',
-						author: {
-							name: `${message.author.tag} (ID ${message.author.id})`,
-							icon_url: message.author.displayAvatarURL({ dynamic: true }),
-						},
-						description: `**Contenu du message**\n${message.cleanContent}`,
-						fields: [
-							{
-								name: 'Channel',
-								value: message.channel,
-								inline: true,
-							},
-							{
-								name: 'Message',
-								value: `[${message.id}](${message.url})`,
-								inline: true,
-							},
-							{
-								name: '1er signalement',
-								value: `Signalement de <@${user.id}> le ${convertDate(new Date())}`,
-								inline: false,
-							},
-						],
-						footer: {
-							text: `Posté le ${convertDate(message.createdAt)}`,
-						},
-					},
+				sendLogReport.embed.color = 'ffae00'
+				sendLogReport.embed.fields.push({
+					name: '1er signalement',
+					value: `Signalement de <@${user.id}> le ${convertDate(new Date())}`,
+					inline: false,
 				})
 				break
 			case 2:
-				reportChannel.send({
-					embed: {
-						color: 'ff8200',
-						author: {
-							name: `${message.author.tag} (ID ${message.author.id})`,
-							icon_url: message.author.displayAvatarURL({ dynamic: true }),
-						},
-						description: `**Contenu du message**\n${message.cleanContent}`,
-						fields: [
-							{
-								name: 'Channel',
-								value: message.channel,
-								inline: true,
-							},
-							{
-								name: 'Message',
-								value: `[${message.id}](${message.url})`,
-								inline: true,
-							},
-							{
-								name: '1er signalement',
-								value: '?',
-								inline: false,
-							},
-							{
-								name: '2nd signalement',
-								value: `Signalement de <@${user.id}> le ${convertDate(new Date())}`,
-								inline: false,
-							},
-						],
-						footer: {
-							text: `Posté le ${convertDate(message.createdAt)}`,
-						},
+				sendLogReport.embed.color = 'ff8200'
+				sendLogReport.embed.fields.push(
+					{
+						name: '1er signalement',
+						value: '?',
+						inline: false,
 					},
-				})
+					{
+						name: '2nd signalement',
+						value: `Signalement de <@${user.id}> le ${convertDate(new Date())}`,
+						inline: false,
+					},
+				)
 				break
 			case 3:
-				reportChannel.send({
-					embed: {
-						color: 'ff6600',
-						author: {
-							name: `${message.author.tag} (ID ${message.author.id})`,
-							icon_url: message.author.displayAvatarURL({ dynamic: true }),
-						},
-						description: `**Contenu du message**\n${message.cleanContent}`,
-						fields: [
-							{
-								name: 'Channel',
-								value: message.channel,
-								inline: true,
-							},
-							{
-								name: 'Message',
-								value: `[${message.id}](${message.url})`,
-								inline: true,
-							},
-							{
-								name: '1er signalement',
-								value: '?',
-								inline: false,
-							},
-							{
-								name: '2nd signalement',
-								value: '?',
-								inline: false,
-							},
-							{
-								name: '3ème signalement',
-								value: `Signalement de <@${user.id}> le ${convertDate(new Date())}`,
-								inline: false,
-							},
-						],
-						footer: {
-							text: `Posté le ${convertDate(message.createdAt)}`,
-						},
+				sendLogReport.embed.color = 'ff6600'
+				sendLogReport.embed.fields.push(
+					{
+						name: '1er signalement',
+						value: '?',
+						inline: false,
 					},
-				})
+					{
+						name: '2nd signalement',
+						value: '?',
+						inline: false,
+					},
+					{
+						name: '3ème signalement',
+						value: `Signalement de <@${user.id}> le ${convertDate(new Date())}`,
+						inline: false,
+					},
+				)
 				break
 			case 4:
-				reportChannel.send({
-					embed: {
-						color: 'ff3200',
-						author: {
-							name: `${message.author.tag} (ID ${message.author.id})`,
-							icon_url: message.author.displayAvatarURL({ dynamic: true }),
-						},
-						description: `**Contenu du message**\n${message.cleanContent}`,
-						fields: [
-							{
-								name: 'Channel',
-								value: message.channel,
-								inline: true,
-							},
-							{
-								name: 'Message',
-								value: `[${message.id}](${message.url})`,
-								inline: true,
-							},
-							{
-								name: '1er signalement',
-								value: '?',
-								inline: false,
-							},
-							{
-								name: '2nd signalement',
-								value: '?',
-								inline: false,
-							},
-							{
-								name: '3ème signalement',
-								value: '?',
-								inline: false,
-							},
-							{
-								name: '4ème signalement',
-								value: `Signalement de <@${user.id}> le ${convertDate(new Date())}`,
-								inline: false,
-							},
-						],
-						footer: {
-							text: `Posté le ${convertDate(message.createdAt)}`,
-						},
+				sendLogReport.embed.color = 'ff3200'
+				sendLogReport.embed.fields.push(
+					{
+						name: '1er signalement',
+						value: '?',
+						inline: false,
 					},
-				})
+					{
+						name: '2nd signalement',
+						value: '?',
+						inline: false,
+					},
+					{
+						name: '3ème signalement',
+						value: '?',
+						inline: false,
+					},
+					{
+						name: '4ème signalement',
+						value: `Signalement de <@${user.id}> le ${convertDate(new Date())}`,
+						inline: false,
+					},
+				)
 				messageReaction.message.delete()
 				break
 			default:
 				messageReaction.message.delete()
 				break
 		}
+		reportChannel.send(sendLogReport)
+	}
 }
