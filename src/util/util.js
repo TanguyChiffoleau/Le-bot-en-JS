fa1a61b20f4444212c29ed6a488288620fb640d3
Tanguy Chiffoleau <55456592+TanguyChiffoleau@users.noreply.github.com>
2020-07-23 12:13:32 +0200

    Suppression espace inutile
    Suppression espace inutile pour respecter la règle "Trailing spaces not allowed"

--- src/util/util.js
+++ src/util/util.js
@@ -21,7 +21,7 @@ module.exports = {
 		}
 
 		const diff = new Date() - date
-		const years = Math.floor(diff / (1000 * 60 * 60 * 24 * 30.4375 * 12)) 
+		const years = Math.floor(diff / (1000 * 60 * 60 * 24 * 30.4375 * 12))
 		const months = Math.floor((diff / (1000 * 60 * 60 * 24 * 30.4375)) % 12)
 		const days = Math.floor(((diff / (1000 * 60 * 60 * 24)) % 365.25) % 30.4375)
 		const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
