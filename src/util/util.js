30b4740ae4d4dbee337ecfc7f76694a6a8ebd50e
alexandre-badoux <43375881+alexandre-badoux@users.noreply.github.com>
2020-07-23 11:24:53 +0200

    Correction 'diffDate'
    Les années, mois et jours se calculent correctement désormais.
    365.25/12 = 30.4375

--- src/util/util.js
+++ src/util/util.js
@@ -21,9 +21,9 @@ module.exports = {
 		}
 
 		const diff = new Date() - date
-		const years = Math.floor(diff / (1000 * 60 * 60 * 24 * 30 * 12))
-		const months = Math.floor((diff / (1000 * 60 * 60 * 24 * 30)) % 12)
-		const days = Math.floor((diff / (1000 * 60 * 60 * 24)) % 365.25)
+		const years = Math.floor(diff / (1000 * 60 * 60 * 24 * 30.4375 * 12)) 
+		const months = Math.floor((diff / (1000 * 60 * 60 * 24 * 30.4375)) % 12)
+		const days = Math.floor(((diff / (1000 * 60 * 60 * 24)) % 365.25) % 30.4375)
 		const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
 		const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60))
 
