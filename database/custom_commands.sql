225e38fbf36691761b008ebe337d11b01a507332
TanguyChiffoleau <55456592+TanguyChiffoleau@users.noreply.github.com>
2020-12-25 10:01:48 +0100

    Utilisation anglais nom colonnes

--- database/custom_commands.sql
+++ database/custom_commands.sql
@@ -2,12 +2,12 @@ CREATE TABLE "Custom commands"(
 	"id" SERIAL PRIMARY KEY,
 	"name" TEXT UNIQUE NOT NULL,
 
-	"texte" TEXT NOT NULL,
+	"text" TEXT NOT NULL,
 	"author_id" TEXT NOT NULL,
 	"created_at" TIMESTAMP WITH TIME ZONE NOT NULL,
 	"last_modified_at" TIMESTAMP NULL,
 	"last_modified_by" TIMESTAMP NULL,
-	"utilisations" INTEGER NOT NULL DEFAULT 0,
+	"uses" INTEGER NOT NULL DEFAULT 0,
 	"categories" TEXT[] NULL,
 	"tags" TEXT[] NULL
 );
