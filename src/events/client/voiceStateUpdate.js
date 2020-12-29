08b693cec65b1e267e3e848c4fe04691cd46bbf8
Emilien212 <51836065+Emilien212@users.noreply.github.com>
2020-12-29 01:33:23 +0100

    Create voiceStateUpdate.js
    ajoute du fichier permettant la création de vocaux personnalisé

---
+++ src/events/client/voiceStateUpdate.js
@@ -0,0 +1,13 @@
+const user_channel = []
+module.exports = async (client, before, after) => {
+    if (after.channelID != null && before.channelID != after.channelID && client.config.voiceCreateCreatorsIDs.includes(after.channelID)){
+        const member = client.guilds.cache.get(client.config.guildID).members.cache.get(after.id)
+        const channel = await client.channels.cache.get(after.channelID).clone({name : `${member.user.username}'s channel`})
+        member.voice.setChannel(channel)
+        user_channel.push(channel.id)
+    }
+    if (before.channelID != null && before.channel.id != after.channelID && user_channel.includes(before.channelID) && client.channels.cache.get(before.channelID).members.size==0){
+        client.channels.cache.get(before.channelID).delete()
+        user_channel.splice(user_channel.indexOf(before.channelID), 1)
+    }
+}
