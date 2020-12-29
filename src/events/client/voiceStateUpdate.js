c24a9b65e13b2ab4f9e7ff96d90afd5991908ca3
Emilien212 <emilienthoreau@gmail.com>
2020-12-29 20:55:04 +0100

    Ajout de la partie gérant les permissions

--- src/events/client/voiceStateUpdate.js
+++ src/events/client/voiceStateUpdate.js
@@ -1,4 +1,6 @@
 const user_channel = []
+const no_mic = {}
+const modo_id_roles = []
 module.exports = async (client, oldState, newState) => {
 	if (
 		newState.channelID &&
@@ -14,11 +16,53 @@ module.exports = async (client, oldState, newState) => {
 	}
 	if (
 		oldState.channelID &&
-		oldState.channel.id !== newState.channelID &&
+		oldState.channelID !== newState.channelID &&
 		user_channel.includes(oldState.channelID) &&
 		client.channels.cache.get(oldState.channelID).members.size === 0
 	) {
 		client.channels.cache.get(oldState.channelID).delete()
 		user_channel.splice(user_channel.indexOf(oldState.channelID), 1)
+		if (no_mic.includes(oldState.channelID)) await no_mic[oldState.channelID].delete()
+	}
+	if (
+		oldState.channelID &&
+		oldState.channelID !== newState.channelID &&
+		user_channel.includes(oldState.channelID) &&
+		oldState.channelID in no_mic
+	)
+		await no_mic[oldState.channelID].permissionOverwrites.get(newState.id).delete()
+	if (
+		newState.channelID &&
+		oldState.channelID !== newState.channelID &&
+		user_channel.includes(newState.channelID) &&
+		newState.channelID in no_mic
+	) {
+		const member = client.guilds.cache.get(client.config.guildID).members.cache.get(newState.id)
+		let set_perm = true
+		for (const role in member.roles) if (modo_id_roles.includes(role.id)) set_perm = false
+		if (set_perm)
+			await no_mic[newState.channelID].overwritePermissions([
+				{
+					id: newState.id,
+					allow: [
+						'VIEW_CHANNEL',
+						'SEND_MESSAGES',
+						'EMBED_LINKS',
+						'ATTACH_FILES',
+						'READ_MESSAGE_HISTORY',
+						'USE_EXTERNAL_EMOJIS',
+						'ADD_REACTIONS',
+					],
+					deny: [
+						'CREATE_INSTANT_INVITE',
+						'MANAGE_CHANNELS',
+						'MANAGE_WEBHOOKS',
+						'SEND_TTS_MESSAGES',
+						'MANAGE_MESSAGES',
+						'EMBED_LINKS',
+						'MENTION_EVERYONE',
+					],
+				},
+			])
 	}
 }
