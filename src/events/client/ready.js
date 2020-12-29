2f51184c40820b5b9a5583db0ce59ebac92ccb01
TanguyChiffoleau <55456592+TanguyChiffoleau@users.noreply.github.com>
2020-12-29 23:33:03 +0100

    Ajout commentaires dans ready

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -8,11 +8,17 @@ module.exports = async client => {
 	// puis ajout des émojis (peut prendre du temps)
 	client.reactionRoleMap = new Map()
 
+	// Pour chaque channel
 	for (const { channelID, messageArray } of reactionRoleConfig) {
+		// Fetch du channel
 		const channel = await client.channels.fetch(channelID)
+		// Pour chaque message/réactions
 		for (const { messageID, emojiRoleMap } of messageArray) {
+			// Ajout dans la map pour être utilisé dans les events
 			client.reactionRoleMap.set(messageID, emojiRoleMap)
+			// Fetch du message
 			const message = await channel.messages.fetch(messageID)
+			// Ajout des émojis sur le message
 			for (const emoji of Object.keys(emojiRoleMap)) await message.react(emoji)
 		}
 	}
