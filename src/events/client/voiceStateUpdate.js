5935be007deefa06400d3677774219ddd7f46d7d
TanguyChiffoleau <55456592+TanguyChiffoleau@users.noreply.github.com>
2021-02-28 21:03:44 +0100

    Réorganisation pour fixer les différents bugs

--- src/events/client/voiceStateUpdate.js
+++ src/events/client/voiceStateUpdate.js
@@ -1,11 +1,52 @@
 module.exports = async (client, oldState, newState) => {
 	if (oldState.channelID === newState.channelID) return
 
+	// S'il quitte un channel personnalisé
+	if (oldState.channel && client.voiceManager.has(oldState.channelID))
+		if (oldState.channel.members.size === 0) {
+			// S'il le channel qu'il a quitté est vide
+
+			// Acquisition du channel nomic
+			const noMicChannel = client.voiceManager.get(oldState.channelID)
+			// S'il existe
+			if (noMicChannel)
+				// On supprime le channel nomic
+				noMicChannel.delete()
+
+			// On supprime le channel de la map
+			client.voiceManager.delete(oldState.channelID)
+
+			// Suppression du channel vocal
+			// Catch si le channel est déjà supprimé
+			oldState.channel.delete().catch(() => null)
+		} else if (client.voiceManager.get(oldState.channelID)) {
+			// S'il n'est pas vide et qu'il quitte un channel avec un no_mic
+
+			// Suppression des permissions du membre pour le channel no_mic
+			client.voiceManager
+				.get(oldState.channelID)
+				.permissionOverwrites.get(newState.id)
+				.delete()
+		}
+
 	// Si l'utilisateur rejoint un channel
 	if (newState.channel) {
 		// S'il rejoint un channel qui doit créer un nouveau channel
 		if (client.config.voiceManagerChannelsIDs.includes(newState.channelID)) {
 			const member = newState.member
+
+			const permissions = newState.channel.permissionOverwrites.clone().set(member, {
+				id: member,
+				type: 'member',
+				allow: [
+					'VIEW_CHANNEL',
+					'CONNECT',
+					'MANAGE_CHANNELS',
+					'MUTE_MEMBERS',
+					'DEAFEN_MEMBERS',
+					'MOVE_MEMBERS',
+				],
+			})
 
 			// Création du channel vocal
 			const createdChannel = await newState.guild.channels.create(
@@ -13,23 +54,17 @@ module.exports = async (client, oldState, newState) => {
 				{
 					type: 'voice',
 					parent: newState.channel.parent,
-					permissionOverwrites: newState.channel.permissionOverwrites.set(member, {
-						id: member,
-						type: 'member',
-						allow: [
-							'VIEW_CHANNEL',
-							'CONNECT',
-							'MANAGE_CHANNELS',
-							'MUTE_MEMBERS',
-							'DEAFEN_MEMBERS',
-							'MOVE_MEMBERS',
-						],
-					}),
+					permissionOverwrites: permissions,
 				},
 			)
 
 			// Déplacement du membre dans son nouveau channel vocal
-			await member.voice.setChannel(createdChannel)
+			const moveAction = await member.voice.setChannel(createdChannel).catch(() => null)
+
+			// Si l'utilisateur ne peut pas être move dans le channel créé,
+			// on supprime le channel créé
+			if (!moveAction) return createdChannel.delete()
+
 			// Ajout de l'id du channel vocal perso dans la liste
 			return client.voiceManager.set(createdChannel.id, null)
 		}
@@ -43,40 +78,7 @@ module.exports = async (client, oldState, newState) => {
 				CREATE_INSTANT_INVITE: false,
 				VIEW_CHANNEL: true,
 				SEND_MESSAGES: true,
+				READ_MESSAGE_HISTORY: true,
 			})
 	}
-
-	// S'il quitte un channel
-
-	// Si le channel quitté est un channel pour créer un channel perso
-	// ou si le channel n'est pas un channel perso on return
-	if (
-		client.config.voiceManagerChannelsIDs.includes(oldState.channelID) ||
-		!client.voiceManager.has(oldState.channelID)
-	)
-		return
-
-	// S'il le channel qu'il a quitté est vide
-	if (oldState.channel.members.size === 0) {
-		const noMicChannel = client.voiceManager.get(oldState.channelID)
-		// S'il avait un channel nomic
-		if (noMicChannel)
-			// On supprime le channel nomic
-			noMicChannel.delete()
-
-		// On supprime le channel de la map
-		client.voiceManager.delete(oldState.channelID)
-
-		// Suppression du channel vocal
-		// Catch si le channel est déjà supprimé
-		return oldState.channel.delete().catch(() => null)
-	}
-
-	// S'il n'est pas vide et qu'il quitte un channel avec un no_mic
-	if (client.voiceManager.get(oldState.channelID))
-		// Suppression des permissions du membre pour le channel no_mic
-		return client.voiceManager
-			.get(oldState.channelID)
-			.permissionOverwrites.get(newState.id)
-			.delete()
 }
