7b1095dd65042075d6cb29c78657f1dc02238fac
55456592+TanguyChiffoleau@users.noreply.github.com <55456592+TanguyChiffoleau@users.noreply.github.com>
2021-03-16 23:50:55 +0100

    Ajout de commentaires pour channelUpdate

--- src/events/client/channelUpdate.js
+++ src/events/client/channelUpdate.js
@@ -1,15 +1,21 @@
 import { GuildChannel, VoiceChannel } from 'discord.js'
 
 export default (client, oldChannel, newChannel) => {
+	// Si le channel n'est pas un channel de guild, return
 	if (!(oldChannel instanceof GuildChannel) || !(newChannel instanceof GuildChannel)) return
 
+	// Si le channel n'est pas un channel vocal, return
 	if (!(oldChannel instanceof VoiceChannel) || !(newChannel instanceof VoiceChannel)) return
 
+	// Si son nom n'a pas changé, return
 	if (oldChannel.name === newChannel.name) return
 
 	const { id: voiceChannelID, name: voiceChannelName } = newChannel
+
+	// Acquisition du channel no mic, et return s'il n'y en a pas
 	const noMicChannel = client.voiceManager.get(voiceChannelID)
 	if (!noMicChannel) return
 
+	// Rename du channel avec no mic + le nouveau nom du vocal
 	return noMicChannel.edit({ name: `no mic ${voiceChannelName}` })
 }
