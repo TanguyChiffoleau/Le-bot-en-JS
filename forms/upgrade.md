f1d0db8ea55307ea22e62793ad1228929853a549
TanguyChiffoleau <55456592+TanguyChiffoleau@users.noreply.github.com>
2021-05-19 23:10:19 +0200

    Ajout d'upgrade

---
+++ forms/upgrade.md
@@ -0,0 +1,10 @@
+```
+Lorsque le test est terminé, copie/colle le lien ici
+**x Votre alim et votre boitier :**
+**x Budget :** (A fixer dès le départ) €
+**x Quand voulez-vous acheter :**
+**x Utilisation :** (Gaming/Montage/Stream...)
+**x Jeux joués :**
+**x Logiciels utilisés :**
+**x Pays de résidence :**
+```
---
+++ src/commands/forms/upgrade.js
@@ -0,0 +1,90 @@
+import { readFile } from 'fs/promises'
+import { Constants } from 'discord.js'
+
+const isUserOnMobileDevice = clientPresenceStatusData => {
+	// Si il n'y a pas d'objet ou un objet vide
+	if (!clientPresenceStatusData || Object.entries(clientPresenceStatusData).length === 0)
+		return true
+
+	// Si il n'est pas sur mobile
+	if (!clientPresenceStatusData.mobile) return false
+
+	// S'il est sur mobile et non AFK
+	if (clientPresenceStatusData.mobile !== 'idle') return true
+
+	// Si AFK sur mobile et online sur une autre platerforme
+	if (clientPresenceStatusData.desktop === 'online' || clientPresenceStatusData.web === 'online')
+		return false
+
+	return true
+}
+
+export default {
+	name: 'upgrade',
+	description: 'Donne le formulaire des upgrade',
+	aliases: ['upgrades'],
+	usage: null,
+	needArguments: false,
+	guildOnly: true,
+	requirePermissions: [],
+	execute: async (client, message) => {
+		const [upgrade, upgradeDescription] = await Promise.all([
+			readFile('./forms/upgrade.md', { encoding: 'utf8' }),
+			readFile('./forms/upgradeDescription.md', { encoding: 'utf8' }),
+		])
+
+		const embed = {
+			color: '#C27C0E',
+			title: 'Formulaire upgrade',
+			author: {
+				name: message.guild.name,
+				icon_url: message.guild.iconURL({ dynamic: true }),
+				url: message.guild.vanityURL,
+			},
+			fields: [
+				{
+					name: 'Channel dans lequel renvoyer le formulaire complété',
+					value: message.guild.channels.cache.get(client.config.configChannelID),
+				},
+				{
+					name: 'Précisions',
+					value: upgradeDescription,
+				},
+			],
+		}
+
+		const targetedMember =
+			!message.mentions.members || !message.mentions.members.size
+				? message.member
+				: message.mentions.members.first()
+
+		const targetedMemberStatus = targetedMember.user.presence.clientStatus
+
+		try {
+			if (isUserOnMobileDevice(targetedMemberStatus)) {
+				await targetedMember.send({ embed })
+				await targetedMember.send(upgrade)
+			} else {
+				embed.description = upgrade
+				await targetedMember.send({ embed })
+			}
+		} catch (error) {
+			if (error.code !== Constants.APIErrors.CANNOT_MESSAGE_USER) throw error
+
+			if (targetedMember === message.member)
+				message.reply(
+					"je n'ai pas réussi à envoyer le message privé, tu as dû sûrement me bloquer/désactiver tes messages provenant du serveur 😬",
+				)
+			else
+				message.reply(
+					"je n'ai pas réussi à envoyer le DM, l'utilisateur mentionné m'a sûrement bloqué /désactivé les messages provenant du serveur 😬",
+				)
+
+			return message.react('❌')
+		}
+
+		if (targetedMember === message.member) return message.delete()
+
+		return message.react('✅')
+	},
+}
