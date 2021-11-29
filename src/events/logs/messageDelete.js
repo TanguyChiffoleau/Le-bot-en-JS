64061ffbfe86ad226872dbbdded5f87980ced0b6
Asthox <thomas.legoff0206@orange.fr>
2021-11-29 16:56:38 +0100

    Prettier

--- src/events/logs/messageDelete.js
+++ src/events/logs/messageDelete.js
@@ -21,7 +21,8 @@ export default async (message, client) => {
 	const logsChannel = message.guild.channels.cache.get(client.config.logsMessagesChannelID)
 	if (!logsChannel) return
 
-	// Vérification si le salon du message est dans la liste des salons à ne pas logger
+	// Vérification si le salon du message 
+	// est dans la liste des salons à ne pas logger
 	if (client.config.noLogsManagerChannelIDs.includes(message.channel.id)) return
 
 	// Fetch du message supprimé
