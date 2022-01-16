3a969a8cf9f95ea0e526ef6e3ea97029a89ec365
Asthox <thomas.legoff0206@orange.fr>
2022-01-16 20:08:58 +0100

    Prettier + simplification

--- src/events/interactions/interactionCreate.js
+++ src/events/interactions/interactionCreate.js
@@ -1,10 +1,10 @@
 export default async (interaction, client) => {
-    if (interaction.isCommand()) {
-        const command = client.commands.get(interaction.commandName)
-        if (!command) {
-            interaction.reply({ content: `Impossible de trouver la commande "${interaction.commandName}"`})
-        } else {
-            command.interaction(interaction, client)
-        }
-    }
+	if (interaction.isCommand()) {
+		const command = client.commands.get(interaction.commandName)
+		if (command) await command.interaction(interaction, client)
+		else
+			return interaction.reply({
+				content: `Impossible de trouver la commande "${interaction.commandName}"`,
+			})
+	}
 }
