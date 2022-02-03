bdb657db94c8649218bd82ff37200df0241e3df4
Asthox <thomas.legoff0206@orange.fr>
2022-02-03 20:22:46 +0100

    Ajustement texte

--- src/events/message/messageCreate.js
+++ src/events/message/messageCreate.js
@@ -188,7 +188,7 @@ export default async (message, client) => {
 			}
 
 			if (validMessage.editedAt)
-				embed.footer.text += `\net édité le ${convertDate(validMessage.editedAt)}`
+				embed.footer.text += `\nÉdité le ${convertDate(validMessage.editedAt)}`
 
 			if (message.author !== validMessage.author) {
 				embed.footer.icon_url = message.author.displayAvatarURL({ dynamic: true })
