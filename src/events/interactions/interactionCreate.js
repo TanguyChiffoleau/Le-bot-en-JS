b3850bf1b1701e05335ea7b27ae119bac17b653e
55456592+TanguyChiffoleau@users.noreply.github.com <55456592+TanguyChiffoleau@users.noreply.github.com>
2022-02-08 22:16:45 +0100

    Fix: prise en compte des permissions

--- src/events/interactions/interactionCreate.js
+++ src/events/interactions/interactionCreate.js
@@ -1,10 +1,19 @@
-export default async (interaction, client) => {
+export default (interaction, client) => {
 	if (interaction.isCommand()) {
 		const command = client.commands.get(interaction.commandName)
-		if (command) await command.interaction(interaction, client)
-		else
+		if (!command)
 			return interaction.reply({
 				content: `Impossible de trouver la commande "${interaction.commandName}"`,
 			})
+
+		if (
+			command.requirePermissions.length > 0 &&
+			interaction.member.permissionsIn(interaction.channel).has(command.requirePermissions)
+		)
+			return interaction.reply({
+				content: "Tu n'as pas les permissions d'effectuer cette commande 😕",
+			})
+
+		return command.interaction(interaction, client)
 	}
 }
