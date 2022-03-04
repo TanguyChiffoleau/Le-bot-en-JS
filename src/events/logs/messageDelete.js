f9ff9a2841b035b02f04f592bb0fd5cf3381f122
Asthox <68952109+Asthox@users.noreply.github.com>
2022-03-04 09:27:20 +0100

    Update src/events/logs/messageDelete.js
    Co-authored-by: Tanguy Chiffoleau <55456592+TanguyChiffoleau@users.noreply.github.com>

--- src/events/logs/messageDelete.js
+++ src/events/logs/messageDelete.js
@@ -39,8 +39,10 @@ export default async (message, client) => {
 	// Si oui alors on archive, sinon on supprime
 	if (message.hasThread) {
 		const thread = await message.thread.fetch()
-		if (thread.messageCount > 1 && !thread.archived) thread.setArchived(true)
-		else thread.delete()
+		if (!thread.archived) {
+				if (thread.messageCount > 1) thread.setArchived(true)
+				else thread.delete()
+		}
 	}
 
 	const logEmbed = {
